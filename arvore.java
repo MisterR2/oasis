b8d67e9dcc5f6127a8a117f0b5b2cb127b8ec64b
Camila de Sousa <cmiladesousa@gmail.com>
2024-12-04 14:43:12 -0300

    finalizando inserir, remover e deletar na árvore

--- arvore.java
+++ arvore.java
@@ -69,9 +69,121 @@ public class Arvore{
             raiz = novoNo;
             return raiz;
         }
+
+        int idRaiz = raiz.getLivro().getId();
+        int idNovo = novoNo.getLivro().getId();
+
+        if(idNovo < idRaiz){
+            raiz.setEsc(inserirNo(raiz.getEsq(), novoNo));
+        } else if(idNovo > idRaiz){
+            raiz.setDir(inserirNo(raiz.getDir(), novoNo));
+        }else{
+            return raiz;
+        }
         
+        int balanco = getBalanco(raiz);
+        int idDirRaiz;
+        int idEscRaiz;
+
+        if(raiz.getDir() != null){
+            idDirRaiz = raiz.getDir().getLivro().getId();
+        }
+        if(raiz.getEsq() != null){
+            idDirRaiz = raiz.getEsq().getLivro().getId();
+        }
+
+        //Esquerda
+        if(balanco > 1 && idNovo < idEscRaiz){
+            return balancoDireita(raiz);
+        }
+
+        //Direita
+        if(balanco < -1 && idNovo > idDirRaiz){
+            return balancoEsquerda(raiz);
+        }
+
+        //esquerda-direita
+        if(balanco > 1 && idNovo > idEscRaiz){
+            raiz.etEsc(balancoEsquerda(raiz->getEsc()));
+            return balancoDireita(raiz);
+        }
+        //direita-esquerda
+        if(balanco < -1 && idNovo < idDirRaiz){
+            raiz.setDir(balancoDireita(raiz->getDir()));
+            return balancoEsquerda(raiz);
+        }
+
+        return raiz;
+    
     }
 
+    public No buscaNo(No raiz, int id){
+          if(raiz == null){
+            return raiz;
+        }
 
+        int idRaiz = raiz.getLivro().getId();
+        if(idRaiz == id){
+            return raiz;
+        }else if(id < idRaiz){
+            return buscaNo(raiz.getEsc(), id);
+        }else{
+            return buscaNo(raiz.getDir(), id);
+        }
+    }
+
+    public No valorminimo(No no){
+        No noAtual = no;
+        while(noAtual.getEsc() != null){
+            noAtual = noAtual.getEsc();
+        }
+
+        return noAtual;
+    }
+
+    public No deletarNo(No raiz, int id){
+        int idRaiz = raiz.getLivro().getId();
+        if(raiz == null){
+            return null;
+        }else if(id < idRaiz){
+            raiz.setEsc(deletarNo(raiz.getEsc(), id));
+        }else if(id > idRaiz){
+            raiz.setDir(deletarNo(raiz.getDir(), id));
+        }else{
+            if(raiz.getEsc() == null){
+                return raiz.getDir(); 
+            } else if(raiz.getDir() == null){
+                return raiz.getEsc();
+            }else{
+                No temp = valorminimo(raiz.getDir());
+                raiz.setLivro(temp.getLivro());
+                raiz.setDir(deletarNo(raiz.getDir(), temp.getLivro().getId()));
+            }
+
+        }
+
+        int balanco = getBalanco(raiz);
+
+        //Esquerda
+        if(balanco == 2 && getBalanco(raiz.getEsc()) >= 0)
+            return balancoDireita(raiz);
+        
+        //Dupla Direita
+        else if (balanco == 2 && getBalanco(raiz.getEsc()) == -1){
+            raiz.setEsc(balancoEsquerda(raiz.getEsc()));
+            return balancoDireita(raiz);
+        }
+        //direita
+        else if(balanco == -2 && getBalanco(raiz.getDir()) <= 0){
+            return balancoEsquerda(raiz);
+        }
+        //dupla esquerda
+        else if(balanco == -2 && getBalanco(raiz.getDir()) == 1){
+            raiz.setDir(balancoDireita(raiz.getDir()));
+            return balancoEsquerda(raiz);
+        }
+
+        return raiz;
+    }
 
 }
